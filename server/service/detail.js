b783a484cd307f25c1b8d8f440c9dc8bb7827d0c
switer <guankaishe@gmail.com>
2015-06-22 13:18:57 +0800

    api request add user-agent

--- server/service/detail.js
+++ server/service/detail.js
@@ -2,19 +2,24 @@ var request = require('request')
 
 var api = 'http://node-hnapi-eu.herokuapp.com/item'
 
-function getDetail(options, callback, times){
-	if (times == undefined) times = 20
-	var postid = options.postid
-    request(api + '/' + postid, function (star, resp, body) {
-    	try {
-    		body = JSON.parse(body)
-    	} catch(e) {
-    		if (times <=0) {
-    			body = {}
-    		} else {
-    			return getDetail(options, callback, times)
-    		}
-    	}
+function getDetail(options, callback, times) {
+    if (times == undefined) times = 20
+    var postid = options.postid
+    request({
+        url: api + '/' + postid,
+        headers: {
+            'User-Agent': 'Mozilla/5.0 (Linux; U; Android 4.0; en-us; GT-I9300 Build/IMM76D) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30'
+        }
+    }, function(star, resp, body) {
+        try {
+            body = JSON.parse(body)
+        } catch (e) {
+            if (times <= 0) {
+                body = {}
+            } else {
+                return getDetail(options, callback, times)
+            }
+        }
         callback(body)
     })
 };
--- server/service/post.js
+++ server/service/post.js
@@ -6,7 +6,12 @@ function getPosts(options, callback, times){
 
     options = options || {}
     if (options.loadmore) api += '2'
-    request(api, function (star, resp, body) {
+    request({
+        url: api,
+        headers: {
+            'User-Agent': 'Mozilla/5.0 (Linux; U; Android 4.0; en-us; GT-I9300 Build/IMM76D) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30'
+        }
+    }, function (star, resp, body) {
         try {
             body = JSON.parse(body)
         } catch(e) {
